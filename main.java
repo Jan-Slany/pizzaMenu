8fdb07ee5264bbe89601b3eae8feff88c6658f5f
slany3.jan <97552526+Jan-Slany@users.noreply.github.com>
2022-09-12 17:03:47 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.printf("Pizza menu: %n1. Spinachi %n2. Salami %n3. Napoletana %n");
+
+        String pizza = scan.nextLine();
+
+        switch (pizza){
+            case "1" -> System.out.println("Spinachi je na cestě!");
+            case "2" -> System.out.println("Salami je na cestě!");
+            case "3" -> System.out.println("Napoletana je na cestě!");
+        }
+
+    }
+}
